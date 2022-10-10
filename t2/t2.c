c893752be433bcdbf6835c155baf9425fe0bd12e
Rafaela da Rosa Soares <desenvolvimento@ersistemas.info>
2022-10-10 16:24:13 -0300

    Inicio trabalho 2

--- t2/t2.c
+++ t2/t2.c
@@ -2,27 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include <stdbool.h>
 
 void tam_matriz(int *lin, int *col);
-void preenche_matriz(char matriz, int lin, int col); //precisa remover espaços em branco
-void imprime_matriz(char matriz, int lin, int col);
-void procura_palavras(char matriz, int lin, int col);
-bool retorna_busca(char matriz, char palavra);
+void preenche_matriz(char* matriz, int lin, int col);
+void remove_espaco(char*matriz, int lin, int col);
+void imprime_matriz(int lin, int col, char matriz);
+void pega_palavra(char* palavra, char* matriz, int lin, int col);
+bool checa_vertical(char matriz, char palavra);
+bool checa_horizontal(char matriz, char palavra);
+bool checa_diagonais(char matriz, char palavra);
+void busca(char matriz, char palavra, int li, int col);
+void funcionamento_geral(char matriz, char palavra, int lin, int col);
 
 int main(){
-
     setlocale(LC_ALL, "Portuguese");
-    int lin, col;
-    
-    printf("CAÇA-PALAVRAS PERSONALIZADO: \n");
-    printf("Preencha as especificações: \n");
-    tam_matriz(&lin, &col);
 
-    char matriz[lin][col];
+    int lin, col;
+    char palavra[81];
 
     setbuf(stdin, NULL);
 
-    preenche_matriz(matriz, lin, col);
+    do{
+        printf("CAÇA-PALAVRAS PERSONALIZADO: \n");
+        printf("Preencha as especificações: \n");
+
+        tam_matriz(&lin, &col);
+
+        char matriz[lin][col];
+
+        do{
+            preenche_matriz(matriz, lin, col);
+            remove_espaco(matriz, lin, col);
+            imprime_matriz(matriz, lin, col);
+            funcionamento_geral(matriz, palavra, lin, col);
+        } while(true);
+
+    } while(true);
+    
+    
 
     return 0;
 }
@@ -37,23 +55,54 @@ void tam_matriz(int *lin, int *col){
 
 }
 
-void preenche_matriz(char matriz, int lin, int col){ //precisa remover espaços em branco
+void preenche_matriz(char* matriz, int lin, int col){
+    printf("\nPreencha a matriz do caça-palavras, seguindo a ordem de %d por %d", lin, col);
 
     for(int i=1; i<=lin; i++){
         for(int j=1; j<=col; j++){
             scanf("[ ^\n]", matriz);
-            if(matriz[i][j] == ' '){
-
-            }
         }
     }
 } 
 
-void procura_palavras(char matriz, int lin, int col){
-
+void remove_espaco(char*matriz, int lin, int col){
 
 }
 
-bool retorna_busca(char matriz, char palavra){
+void imprime_matriz(int lin, int col, char matriz){
+    for(int i=0; i<lin; i++){
+        for(int j=0; j<lin; j++){
+            printf("%c", matriz[i][j]);
+        }
+    }
+}
 
+char pega_palavra(char* palavra, char matriz, int lin, int col){
+    printf("\nEscreva a palavra a ser encontrada: \n");
+    scanf(" %[^\n]", palavra);
+    int size = strlen(palavra);
+
+    for(int i=0; i<lin; i++){
+        for(int j=0; j<col; j++){
+            if (size <= lin*col){
+                
+            }
+        }
+    }
+}
+
+
+
+void busca(char matriz, char palavra, int li, int col){
+    for (int i=0; i<lin; i++){
+        for(int j=0; j<col; j++){
+
+        }
+    }
+}
+void funcionamento_geral(char matriz, char palavra, int lin, int col){
+    do{
+        pega_palavra(palavra, matriz, lin, col);
+        
+    }while(true);
 }

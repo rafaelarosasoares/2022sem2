9d30404369847520b87e435acbd3387b156c48cf
Rafaela da Rosa Soares <desenvolvimento@ersistemas.info>
2022-10-20 18:49:09 -0300

    Término Imprimir matriz

--- t2/t2.c
+++ t2/t2.c
@@ -5,18 +5,17 @@
 #include <string.h>
 #include <locale.h>
 #include <stdbool.h>
-#include <ctype.h>
 
 void tam_matriz(int *lin, int *col);
 char** aloca_matriz(int lin, int col);
-char recebe_string(int lin, int col);
-void preenche_matriz(char** matriz, char string, int lin, int col);
+const char* recebe_string(int lin, int col);
+void preenche_matriz(char** matriz, const char* string, int lin, int col);
 void imprime_matriz(char** matriz, int lin, int col);
 void pega_palavra(char* palavra, int lin, int col);
-//bool checa_diagonais(char* palavra, char** matriz, int lin, int col);
-//bool checa_horizontais(char* palavra, char** matriz, int lin, int col);
-//bool checa_verticais(char* palavra, char** matriz, int lin, int col);
-//void retorna_posicao(char** matriz, char* palavra, int lin, int col);
+bool checa_diagonais(char* palavra, char** matriz, int lin, int col);
+bool checa_horizontais(char* palavra, char** matriz, int lin, int col);
+bool checa_verticais(char* palavra, char** matriz, int lin, int col);
+void retorna_posicao(char** matriz, char* palavra, int lin, int col);
 void funcionamento_geral(char** matriz, char* palavra, int lin, int col);
 void dealloc(char** matriz, int lin);
 
@@ -24,12 +23,14 @@ int main(){
     setlocale(LC_ALL, "Portuguese");
 
     int lin, col;
-    char palavra, string;
+    char palavra;
+    const char* string;
     char** matriz;
-
+    int fim;
+    
     setbuf(stdin, NULL);
 
-    while(true){
+    do{
         printf("CAÇA-PALAVRAS PERSONALIZADO: \n");
         printf("Preencha as especificações: \n");
         tam_matriz(&lin, &col);
@@ -37,12 +38,15 @@ int main(){
         while(true){
             matriz = aloca_matriz(lin, col);
             string = recebe_string(lin, col);
-            preenche_matriz(string, matriz, lin, col);
+            preenche_matriz(matriz, string, lin, col);
             imprime_matriz(matriz, lin, col);
-            //funcionamento_geral(matriz, palavra, lin, col);
+            funcionamento_geral(matriz, palavra, lin, col);
             dealloc(matriz, lin);
         }
-    }
+
+        printf("Deseja parar de jogar caça-palavras? Digite 0 se sim, 1 se não.");
+        scanf("%d", &fim);   
+    }while (fim);
 
     return 0;
 }
@@ -54,36 +58,39 @@ void tam_matriz(int *lin, int *col){
 
     printf("\nTamanho da coluna: ");
     scanf("%d", &*col);
-
 }
 
 char** aloca_matriz(int lin, int col){
     char **matriz = (char**)malloc(lin * sizeof(char*));
-    for(int i=0; i<lin; i++)
+    for(int i = 0; i < lin; i++)
         matriz[i] = (char*)malloc(col * sizeof(char));
 
     return matriz;
 } 
 
-char recebe_string(int lin, int col){
-    char string[lin*col];
-    printf("\nInsira texto para guardar na matriz: ");
-    scanf("%s", string);
+const char* recebe_string(int lin, int col){
+    char *string[lin * col];
 
-    for(int i = 0; i < lin * col; i++ ){
-        if(string[i] == ' '){
-            string[i] == '8';
+    printf("\nInsira texto para guardar na matriz: ");
+    scanf(" %[^\n]", *string);
+    
+    unsigned int i, j;
+
+    for(i = 0; i < strlen(*string); i++ ){
+        if(*string[i] == ' '){
+            for(j = i; j < strlen(*string); j++){
+                *string[j] = *string[j+1];
+            }
         }
     }
-
-    return string;
+    return *string;
 }
 
-void preenche_matriz(char** matriz, char string, int lin, int col){
+void preenche_matriz(char** matriz, const char* string, int lin, int col){
     int aux = 0;
 
-    for(int i=0; i<lin; i++){
-        for(int j=0; j<col; j++){
+    for(int i = 0; i < lin; i++){
+        for(int j = 0; j < col; j++){
             if(string[aux] != '\0' && string[aux] != 32) matriz[i][j] = string[aux];
             if(string[aux] != '\0') aux++;
             if(string[aux] == 32) j--;
@@ -91,10 +98,9 @@ void preenche_matriz(char** matriz, char string, int lin, int col){
     }
 }
 
-
 void imprime_matriz(char** matriz, int lin, int col){
-    for(int i=0; i<lin; i++){
-        for(int j=0; j<col; j++) printf("%c", matriz[i][j]);
+    for(int i = 0; i < lin; i++){
+        for(int j = 0; j < col; j++) printf("%c", matriz[i][j]);
         printf("\n");
     }
 }
@@ -107,9 +113,7 @@ void pega_palavra(char* palavra, int lin, int col){
     strcpy(palavra, string);
 }
 
-/*
 void funcionamento_geral(char** matriz, char* palavra, int lin, int col){ 
-    int posicao = 0;
 
     while(true){
         pega_palavra(palavra, lin, col);
@@ -119,9 +123,8 @@ void funcionamento_geral(char** matriz, char* palavra, int lin, int col){
     }
     
 }
-*/
 
 void dealloc(char** matriz, int lin){
-    for(int i=0; i<lin; i++) free(matriz[i]);
+    for(int i = 0; i < lin; i++) free(matriz[i]);
     free(matriz); 
 }
